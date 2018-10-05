353f039b1bba9e33783bc3b19184df05a96f97b5
harish <harishbn31@gmail.com>
2018-10-05 19:42:17 +0530

    key

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const CONNECTION_URI = 'mongodb://Harishbn31:harrybn_31@ds223653.mlab.com:23653/rsvp_data'
+const CONNECTION_URI = 'mongodb://Harishbn31:harrybn_31@ds223653.mlab.com:23653/rsvp_data?authSource=rsvp_data&w=1'
 mongoose.connect(CONNECTION_URI,{useNewUrlParser: true}).then(()=>{
     console.log('connected to db')
 }).catch((err) => {
