9fdb67e22f7741b4aed68dacfdb423ebbfcef770
harish <harishbn31@gmail.com>
2018-10-05 20:22:28 +0530

    port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(morgan('short'));
 app.use('/',router)
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`listening on port ${PORT}`)
 });
