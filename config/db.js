29d5543709d63ac4b5e3dd3f680112ee74af43c4
harish <harishbn31@gmail.com>
2018-10-05 19:32:40 +0530

    production added

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const CONNECTION_URI = 'mongodb://harishbn31:harrybn_31@ds223653.mlab.com:23653/rsvp_data' || 'mongodb://localhost:27017/rsvp_data'
+const CONNECTION_URI = 'mongodb://harishbn31:harrybn_31@ds223653.mlab.com:23653/rsvp_data'
 mongoose.connect(CONNECTION_URI,{useNewUrlParser: true}).then(()=>{
     console.log('connected to db')
 }).catch((err) => {
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(morgan('short'));
 app.use('/',router)
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT,()=>{
     console.log(`listening on port ${PORT}`)
 });
