656d395fe3faf376dc38bc7b8ef1a59b3f570f3f
harish <harishbn31@gmail.com>
2018-10-05 18:27:04 +0530

    mlab

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/rsvp_data'
+
+const CONNECTION_URI = 'mongodb://harishbn31:harrybn_31@ds223653.mlab.com:23653/rsvp_data' || 'mongodb://localhost:27017/rsvp_data'
 mongoose.connect(CONNECTION_URI,{useNewUrlParser: true}).then(()=>{
     console.log('connected to db')
 }).catch((err) => {
