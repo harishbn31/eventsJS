984b8bf09164e97600ec05a71814caa85f6076e0
harish <harishbn31@gmail.com>
2018-10-06 15:08:40 +0530

    check db

--- config/db.js
+++ config/db.js
@@ -2,13 +2,16 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const CONNECTION_URI = 'mongodb://harish:harrybn_31@ds223653.mlab.com:23653/rsvp_data'
+// const CONNECTION_URI = 'mongodb://harish:harrybn_31@ds223653.mlab.com:23653/rsvp_data' || 'mongodb://localhost:27017/rsvp_data'
+// let process.env.MONGODB_URI = 'mongodb://harish:harrybn_31@ds223653.mlab.com:23653/rsvp_data'
+const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/rsvp_data'
+mongoose.set('debug',true)
 mongoose.connect(CONNECTION_URI,{useNewUrlParser: true}).then(()=>{
     console.log('connected to db')
 }).catch((err) => {
     console.log(err)
 })
-
+mongoose.set('useCraeteIndex',true);
 module.exports = {
     mongoose
 }
