8982e5dc4a4640a450e9b164080980a556d3a0bf
harish <harishbn31@gmail.com>
2019-01-29 13:38:18 +0530

    static page

--- config/routes.js
+++ config/routes.js
@@ -7,7 +7,9 @@ const {regController} = require('../app/controllers/registrationsController')
 // router.use('/',h_controller);
 // router.use(express.static(path.join(__dirname, 'react/build')));
 router.get('/', (req, res) => {
-    res.send("Hello")
+    // res.send("Hello")
+    // res.sendFile('index.html' , { root : 'app/react/build'});
+    res.sendFile('main.html' , { root : 'app/react/build'});
   });
 router.use('/events',eventsController)
 router.use('/registrations',regController)
