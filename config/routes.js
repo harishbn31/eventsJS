5167e90664e872b04b48ea77b0103c948fd25947
harish <harishbn31@gmail.com>
2019-01-29 14:35:18 +0530

    server changes

--- config/routes.js
+++ config/routes.js
@@ -6,11 +6,6 @@ const {eventsController} = require('../app/controllers/eventsController')
 const {regController} = require('../app/controllers/registrationsController') 
 // router.use('/',h_controller);
 // router.use(express.static(path.join(__dirname, 'react/build')));
-router.get('/', (req, res) => {
-    // res.send("Hello")
-    // res.sendFile('index.html' , { root : 'app/react/build'});
-    res.sendFile('index.html' , { root : 'app/react/build'});
-  });
 router.use('/events',eventsController)
 router.use('/registrations',regController)
 module.exports = {
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 const morgan = require('morgan');
 const {router} = require('./config/routes');
 const mongoose = require('./config/db');
@@ -8,7 +9,11 @@ app.use(cors())
 app.use(express.json());
 
 app.use(morgan('short'));
-app.use('/',router)
+// app.use('/',router)
+app.use(express.static(path.join(__dirname, 'app/react/build')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'app/react/build/index.html'));
+  });
 // app.use(express.static(root,'./app/index.html'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
