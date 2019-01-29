d87798e6ca805529e66091f8d26dcaf45038ca94
harish <harishbn31@gmail.com>
2019-01-29 12:06:02 +0530

    react success

--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const {h_controller} = require('../app/controllers/home_controller');
 const {eventsController} = require('../app/controllers/eventsController')
 const {regController} = require('../app/controllers/registrationsController') 
 // router.use('/',h_controller);
-router.use(express.static(path.join(__dirname, 'react/build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/react/build/index.html'));
+router.use(express.static(path.join(__dirname, '../app/react/build')));
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'../app/react/build/index.html'));
   });
   router.use('/events',eventsController)
 router.use('/registrations',regController)
